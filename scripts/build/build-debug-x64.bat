f3ddf938fdbb9a481401cffdc6fd7456e7ec04f9
DP458 <Dimka458@yandex.ru>
2019-08-13 12:54:20 +0300

    Deleted scripts folder

--- scripts/build/build-debug-x64.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-msbuild /nologo ..\..\src\ClipboardManager.sln /t:Rebuild /p:"Configuration=Debug" /p:"Platform=x64"
--- scripts/build/build-release-x86.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-msbuild /nologo ..\..\src\ClipboardManager.sln /t:Rebuild /p:"Configuration=Release" /p:"Platform=x86"
